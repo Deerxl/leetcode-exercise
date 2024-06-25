5b602431164efc41a986ff896eca1372a05a3d0a
jialu.yxl <jialu.yxl@alibaba-inc.com>
2024-06-25 17:08:59 +0800

    add

--- src/main/java/org/example/leetcode/StringAndArray.java
+++ src/main/java/org/example/leetcode/StringAndArray.java
@@ -10,7 +10,53 @@ import java.util.stream.Collectors;
 public class StringAndArray {
 
     public static void main(String[] args) {
-        System.out.println(fourSum(new int[] {0,0,0,1000000000,1000000000,1000000000,1000000000},1000000000));
+
+
+        System.out.println(compress(new char[] {'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'}));
+    }
+
+    /**
+     * <a href="https://leetcode.cn/problems/string-compression/">443. String Compression</a>
+     * Given an array of characters chars, compress it using the following algorithm:
+     *
+     * Begin with an empty string s. For each group of consecutive repeating characters in chars:
+     *
+     * If the group's length is 1, append the character to s.
+     * Otherwise, append the character followed by the group's length.
+     * The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+     *
+     * After you are done modifying the input array, return the new length of the array.
+     *
+     * You must write an algorithm that uses only constant extra space.
+     * Input: chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+     * Output: Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
+     * Explanation: The groups are "a" and "bbbbbbbbbbbb". This compresses to "ab12".
+     * @param chars 1 <= chars.length <= 2000
+     * @return
+     */
+    public static int compress(char[] chars) {
+        int putIndex = 0;
+        int i = 0;
+        while (i < chars.length) {
+            int start = i;
+            chars[putIndex++] = chars[i++];
+
+            if (i >= chars.length) {
+                break;
+            }
+
+            if (chars[i] == chars[i - 1]) {
+                while (i < chars.length && chars[i] == chars[i - 1]) {
+                    i++;
+                }
+                String len = String.valueOf(i - start);
+                for (int j = 0; j < len.length(); j++) {
+                    chars[putIndex++] = len.charAt(j);
+                }
+            }
+        }
+
+        return putIndex;
     }
 
     /**
