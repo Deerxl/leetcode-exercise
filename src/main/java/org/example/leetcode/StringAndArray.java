60607e346ea2845bdadb314a470883daca25a4c5
jialu.yxl <jialu.yxl@alibaba-inc.com>
2024-08-08 16:44:17 +0800

    add

--- src/main/java/org/example/leetcode/StringAndArray.java
+++ src/main/java/org/example/leetcode/StringAndArray.java
@@ -16,6 +16,48 @@ public class StringAndArray {
 
 
     /**
+     * <a href="https://leetcode.cn/problems/letter-combinations-of-a-phone-number/">17. Letter Combinations of a Phone Number</a>
+     * Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+     *
+     * A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+     * @param digits 0 <= digits.length <= 4
+     * @return
+     */
+    List<String> letterCombinationsResult = new ArrayList<>();
+    public List<String> letterCombinations(String digits) {
+        if (digits == null || digits.isEmpty()) {
+            return letterCombinationsResult;
+        }
+        Map<Character, List<Character>> map = new HashMap<>();
+        map.put('2', Arrays.asList('a', 'b', 'c'));
+        map.put('3', Arrays.asList('d', 'e', 'f'));
+        map.put('4', Arrays.asList('g', 'h', 'i'));
+        map.put('5', Arrays.asList('j', 'k', 'l'));
+        map.put('6', Arrays.asList('m', 'n', 'o'));
+        map.put('7', Arrays.asList('p', 'q', 'r', 's'));
+        map.put('8', Arrays.asList('t', 'u', 'v'));
+        map.put('9', Arrays.asList('w', 'x', 'y', 'z'));
+
+
+        letterCombinations(digits, 0, map, new StringBuilder());
+        return letterCombinationsResult;
+    }
+
+    private void letterCombinations(String digits, int start, Map<Character, List<Character>> map, StringBuilder builder) {
+        if (start == digits.length()) {
+            letterCombinationsResult.add(builder.toString());
+            return;
+        }
+
+        List<Character> letters = map.get(digits.charAt(start));
+        for (Character letter : letters) {
+            builder.append(letter);
+            letterCombinations(digits, start + 1, map, builder);
+            builder.deleteCharAt(builder.length() - 1);
+        }
+    }
+
+    /**
      * <a href="https://leetcode.cn/problems/combinations/">77. Combinations</a>
      * Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
      *
