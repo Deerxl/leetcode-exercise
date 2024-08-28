9e1c561f64e2000e05bb1478df197f636a280cd0
xiaolu <deer.xl819@gmail.com>
2024-08-28 14:02:55 +0800

    239 利用优先级队列解滑动窗口最大值

--- src/main/java/org/example/leetcode/other/ReviewTopHot.java
+++ src/main/java/org/example/leetcode/other/ReviewTopHot.java
@@ -19,6 +19,41 @@ public class ReviewTopHot {
 
 
     /**
+     * 滑动窗口最大值
+     * <a href="https://leetcode.cn/problems/sliding-window-maximum/description/">239. Sliding Window Maximum</a>
+     * Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+     * Output: [3,3,5,5,6,7]
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (k == 1) {
+            return nums;
+        }
+        Queue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o2[0] - o1[0];
+            }
+        });
+        for (int i = 0; i < k - 1; i++) {
+            queue.offer(new int[]{nums[i], i});
+        }
+
+        int[] result = new int[nums.length - k + 1];
+        for (int i = k - 1; i < nums.length; i++) {
+            while (!queue.isEmpty() && queue.peek()[1] <= i - k) {
+                queue.poll();
+            }
+            queue.offer(new int[]{nums[i], i});
+            result[i - k + 1] = queue.peek()[0];
+        }
+
+        return result;
+    }
+
+    /**
      * 后序遍历
      * @param root
      * @return
