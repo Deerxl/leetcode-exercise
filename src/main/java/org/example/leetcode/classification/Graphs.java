fad5f7c9621e19de3ef239229c382eb7a153d680
xiaolu <deer.xl819@gmail.com>
2024-09-28 22:24:25 +0800

    130. 从边界向内扩展，岛屿题变形

--- src/main/java/org/example/leetcode/classification/Graphs.java
+++ src/main/java/org/example/leetcode/classification/Graphs.java
@@ -12,7 +12,100 @@ public class Graphs {
         int[][] nums = new int[][] {
                 {1,0}
         };
-        System.out.println(canFinish(2, nums));
+
+        char[][] board = new char[][] {
+                {'X','X','X','X'},
+                {'X','O','X','X'},
+                {'X','O','O','X'},
+                {'X','O','X','X'}
+        };
+
+        solve(board);
+        System.out.println(Arrays.deepToString(board));
+    }
+
+    /**
+     * <a href="https://leetcode.cn/problems/surrounded-regions/">130. Surrounded Regions</a>
+     * You are given an m x n matrix board containing letters 'X' and 'O', capture regions that are surrounded:
+     *
+     * Connect: A cell is connected to adjacent cells horizontally or vertically.
+     * Region: To form a region connect every 'O' cell.
+     * Surround: The region is surrounded with 'X' cells if you can connect the region with 'X' cells and none of the region cells are on the edge of the board.
+     * A surrounded region is captured by replacing all 'O's with 'X's in the input matrix board.
+     * @param board
+     */
+    public static void solve(char[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        if (m == 1 || n <= 1) {
+            return;
+        }
+        boolean[][] visited = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'O') {
+                checkMarginO(board, m, n, i, 0, visited);
+            }
+            if (board[i][n - 1] == 'O') {
+                checkMarginO(board, m, n, i, n - 1, visited);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (board[0][i] == 'O') {
+                checkMarginO(board, m, n, 0, i, visited);
+            }
+            if (board[m - 1][i] == 'O') {
+                checkMarginO(board, m, n, m - 1, i, visited);
+            }
+        }
+
+        for (int i = 1; i < m - 1; i++) {
+            for (int j = 1; j < n - 1; j++) {
+                if (board[i][j] == 'O' && !visited[i][j]) {
+                    fillSurroundedX(board, m, n, i, j);
+                }
+            }
+        }
+    }
+
+    private static void fillSurroundedX(char[][] board, int m, int n, int i, int j) {
+        if (i <= 0 || i >= m - 1 || j <= 0 || j >= n - 1) {
+            return;
+        }
+
+        board[i][j] = 'X';
+
+        int[][] dirs = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+        for (int[] dir : dirs) {
+            int x = i + dir[0];
+            int y = j + dir[1];
+            if (x <= 0 || x >= m - 1 || y <= 0 || y >= n - 1) {
+                continue;
+            }
+            if (board[x][y] == 'O') {
+                fillSurroundedX(board, m, n, x, y);
+            }
+        }
+    }
+
+    private static void checkMarginO(char[][] board, int m, int n, int i, int j, boolean[][] visited) {
+        if (visited[i][j]) {
+            return;
+        }
+
+        visited[i][j] = true;
+
+        int[][] dirs = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        for (int[] dir : dirs) {
+            int x = i + dir[0];
+            int y = j + dir[1];
+            if (x < 0 || x >= m || y < 0 || y >= n) {
+                continue;
+            }
+            if (board[x][y] == 'O' && !visited[x][y]) {
+                checkMarginO(board, m, n, x, y, visited);
+            }
+        }
     }
 
     /**
