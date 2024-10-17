7c9a7cb0e7852ff811df5f154e38ff85effbd0da
xiaolu <deer.xl819@gmail.com>
2024-10-17 19:21:57 +0800

    238. top hot 100. 求数组除i外的所有数的乘积，用左右两个数组分别记录[0,i)和(i,len-1]的乘积，再求乘积即可

--- src/main/java/org/example/leetcode/classification/Maths.java
+++ src/main/java/org/example/leetcode/classification/Maths.java
@@ -14,6 +14,44 @@ public class Maths {
 
 
     /**
+     * <a href="https://leetcode.cn/problems/product-of-array-except-self/description/?envType=study-plan-v2&envId=top-100-liked">238. Product of Array Except Self</a>
+     * Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+     *
+     * The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+     *
+     * You must write an algorithm that runs in O(n) time and without using the division operation.
+     * Example 1:
+     *
+     * Input: nums = [1,2,3,4]
+     * Output: [24,12,8,6]
+     * @param nums 2 <= nums.length <= 105
+     * @return
+     */
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+
+        int[] leftMul = new int[nums.length];
+        int[] rightMul = new int[nums.length];
+
+        leftMul[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            leftMul[i] = leftMul[i - 1] * nums[i - 1];
+        }
+
+        rightMul[nums.length - 1] = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            rightMul[i] = rightMul[i + 1] * nums[i + 1];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = leftMul[i] * rightMul[i];
+        }
+
+        return result;
+    }
+
+
+    /**
      * <a href="https://leetcode.cn/problems/rotate-array/?envType=study-plan-v2&envId=top-100-liked">189. Rotate Array</a>
      * Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
      * @param nums 1 <= nums.length <= 105 -231 <= nums[i] <= 231 - 1
