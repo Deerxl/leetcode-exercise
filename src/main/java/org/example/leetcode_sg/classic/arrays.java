f10ca758538f3200d917d7419dad809ca77f1dbf
Deerxl <985912201@qq.com>
2026-09-06 17:47:51 +0800

    top interviews 150
    Change-Id: I2a106c18142e94d8bf515d7d085f449e0a8deee5

--- src/main/java/org/example/leetcode_sg/classic/arrays.java
+++ src/main/java/org/example/leetcode_sg/classic/arrays.java
@@ -10,6 +10,179 @@ public class arrays {
     }
 
     /**
+     * <a href="https://leetcode.com/problems/best-time-to-buy-and-sell-stock/?envType=study-plan-v2&envId=top-interview-150">121. Best Time to Buy and Sell Stock</a>
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) {
+        if (prices.length <= 1) {
+            return 0;
+        }
+        int result = 0;
+        int minPrice = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                result = Math.max(result, prices[i] - minPrice);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/rotate-array/description/?envType=study-plan-v2&envId=top-interview-150">189. Rotate Array</a>
+     * @param nums
+     * @param k
+     *
+     * quite tricky, with space complexity: O(1), reverse 3 times.
+     */
+    public void rotate(int[] nums, int k) {
+        if (nums.length == 1 || k == 0) {
+            return;
+        }
+        k = k % nums.length;
+        if (k == 0) {
+            return;
+        }
+
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, k, nums.length - 1);
+        reverse(nums, 0, k - 1);
+    }
+
+
+    public void reverse(int[] nums, int start, int end) {
+        int tmp;
+        while (start < end) {
+            tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start++;
+            end--;
+        }
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/majority-element/?envType=study-plan-v2&envId=top-interview-150">169. Majority Element</a>
+     * @param nums
+     * @return
+     *
+     * Moore Voting Algorithm
+     */
+    public int majorityElement(int[] nums) {
+        if (nums.length == 2) {
+            return nums[0];
+        }
+        int count = 0;
+        int num = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == num) {
+                count++;
+            } else if (count == 0) {
+                count++;
+                num = nums[i];
+            } else {
+                count--;
+            }
+        }
+        return num;
+    }
+
+
+    /**
+     * <a href="https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/description/?envType=study-plan-v2&envId=top-interview-150">80. Remove Duplicates from Sorted Array II</a>
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates2(int[] nums) {
+        if (nums.length <= 2) {
+            return nums.length;
+        }
+        int currentVal = Integer.MAX_VALUE;
+        int occurTimes = 0;
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (currentVal != nums[i]) {
+                nums[k] = nums[i];
+                k++;
+                currentVal = nums[i];
+                occurTimes = 1;
+            } else if (occurTimes < 2) {
+                occurTimes += 1;
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
+
+    /**
+     * <a href="https://leetcode.com/problems/remove-duplicates-from-sorted-array/?envType=study-plan-v2&envId=top-interview-150">26. Remove Duplicates from Sorted Array</a>
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 1) {
+            return 1;
+        }
+        int currentVal = Integer.MAX_VALUE;
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != currentVal) {
+                nums[k] = nums[i];
+                currentVal = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/remove-element/description/?envType=study-plan-v2&envId=top-interview-150">27. Remove Element</a>
+     * @param nums
+     * @param val
+     * @return
+     */
+    public int removeElement(int[] nums, int val) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
+    /**
+     * <a href="https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150">88. Merge Sorted Array</a>
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index = m + n - 1;
+        n -= 1;
+        m -= 1;
+        while (n >= 0) {
+            if (m < 0 || nums2[n] >= nums1[m]) {
+                nums1[index] = nums2[n];
+                n--;
+            } else {
+                nums1[index] = nums1[m];
+                m--;
+            }
+            index--;
+        }
+    }
+
+    /**
      * <a href="https://neetcode.io/problems/meeting-schedule/question">252. Meeting Rooms</a>
      * time complexity: O(NlogN); space complexity: O(N)
      * @param intervals
